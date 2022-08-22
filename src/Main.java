f2d227c4d26deb7c915320b8e93b4d0f29d9cb6a
sakhaforce1995 <nurgun-andreev@mail.ru>
2022-08-22 22:03:09 +0900

    выполнил дз номер 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Пишем наше дз!!");
+        System.out.println("Делаем коммит и пуш");
+        System.out.println("Переходим в гитхаб");
     }
 }
